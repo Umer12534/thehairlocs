fe0e61418420aa3cf153b3220f0ba75be77dd0b9
Syed Umer <133738875+Umer12534@users.noreply.github.com>
2025-12-29 16:48:56 +0500

    improve responsive designe

--- src/components/sections/allproducts/Allproducts.js
+++ src/components/sections/allproducts/Allproducts.js
@@ -1,23 +1,38 @@
 import FilterSidebar from '../../ui/filtersidebar/FilterSidebar'
+import { useState } from 'react'
 import './Allproducts.css'
 import {products} from '../../../data/Products'
 import ProductCard from '../../ui/ProductCard/ProductCard'
+import Pagination from '@mui/material/Pagination';
+import FilterBar from '../../ui/filterbar/FilterBar'
+
 function Allproducts(){
+    const [isSidebarActive, setIsSidebarActive] = useState(false);
+    
+    const toggleSidebar = () => {
+        setIsSidebarActive(prev => !prev);
+    }
 
     return(
         <>
+        <FilterBar toggleSidebar = {toggleSidebar}/>
+
         <div className="product-page">
-            <div className="filtersidebar">
-                <FilterSidebar/>
+            <div className={`filtersidebar ${isSidebarActive ? 'active' : ''}`}>
+                <FilterSidebar  toggleSidebar = {toggleSidebar}/>
             </div>
             <div className="allproductsection">
-                        {products.map(product => (
-                            <ProductCard
-                                key={product.id}
-                                {...product}
-                            />
-                        ))}
-                    </div>
+                {products.map(product => (
+                    <ProductCard
+                        key={product.id}
+                        {...product}
+                    />
+                ))}
+            </div>
+        </div>
+        {/* Pagination */}
+        <div className="pagination-wrapper">
+            <Pagination count={5} variant="outlined" shape="rounded" />
         </div>
         </>    
     )
