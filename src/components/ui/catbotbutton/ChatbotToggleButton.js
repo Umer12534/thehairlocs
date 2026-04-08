958b072297011da21f5ca2d6a13ad157df2b5784
Syed Umer <133738875+Umer12534@users.noreply.github.com>
2026-04-08 16:38:14 +0500

    improve style chat bot toggele button

--- src/components/ui/catbotbutton/ChatbotToggleButton.js
+++ src/components/ui/catbotbutton/ChatbotToggleButton.js
@@ -2,9 +2,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMessage, faXmark } from "@fortawesome/free-solid-svg-icons";
 import "./ChatbotToggleButton.css";
 
-export default function ChatbotToggleButton({ isChatOpen, onToggle }) {
+export default function ChatbotToggleButton({
+  isChatOpen,
+  isPreviewActive = false,
+  onToggle,
+}) {
   return (
-    <div className={`chat-btn-wrapper ${isChatOpen ? "open-state" : ""}`}>
+    <div
+      className={`chat-btn-wrapper ${isChatOpen ? "open-state" : ""} ${
+        isPreviewActive ? "preview-state" : ""
+      }`}
+    >
       <button
         type="button"
         className="chat-btn"
